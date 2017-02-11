d490bbaa27f55f623fed82e3bef5eb793c55fea7
Anniel Menezes Cruz <contato@annielmenezes.com>
2017-02-10 22:09:32 -0200

    Update bin.sh
    Remove Vagrant installation from file

--- 64bits/bin.sh
+++ 64bits/bin.sh
@@ -13,11 +13,6 @@ wget https://www.dropbox.com/download?dl=packages/ubuntu/$DROPBOX_VERSION_amd64.
 dpkg -i $DROPBOX_VERSION_amd64.deb
 rm -f $DROPBOX_VERSION_amd64.deb
 
-echo "Installing $VAGRANT_VERSION 64bits"
-wget https://dl.bintray.com/mitchellh/vagrant/$VAGRANT_VERSION_x86_64.deb
-dpkg -i $VAGRANT_VERSION_x86_64.deb
-rm -f $VAGRANT_VERSION_x86_64.deb
-
 echo "Installing $ATOM_VERSION 64bits"
 wget http://c758482.r82.cf2.rackcdn.com/$ATOM_VERSION-amd64.deb
 wget https://atom-installer.github.com/$ATOM_VERSION/atom-amd64.deb
